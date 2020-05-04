3686ac7a890c95cf3c20d1e79c9e09f18563e1c2
Kevchan- <kevinfb92@gmail.com>
2020-05-04 18:01:07 +0200

    Update and pagination

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -58,12 +58,109 @@ class VideoController extends Controller
         return response()->json($data, $data['code']);
     }
     
-    public function detail($id){
+     public function update(Request $request, $id){
+        $data = array(
+            'status' => 'error',
+            'code' => 400,
+            'message' => 'El video no se ha actualizado'
+        );        
+      
+        $token = $request->header('Authorization');
+        $jwtAuth = new \JwtAuth();
+        $user = $jwtAuth->checkToken($token, true);
+        $user_id = $user->sub;
+
+        $json = $request->input('json', null);
+        $params = json_decode($json); //objeto
+        $params_array = json_decode($json, true); //array
         
+        if(!empty($params) && !empty($params_array)){
+            $validate = \Validator::make($params_array, [
+                'title'=>'required',
+                'description'=>'required',
+            ]);         
+            
+            if(!$validate->fails()){
+                $video = Video::find($id);
+                if(is_object($video)){
+                    if($video.user_id == $user_id){
+                       $video->title = $params->title;
+                       $video->description = $params->description;
+                       $video->save();
+                  
+                        $data = array(
+                            'status' => 'success',
+                            'code' => 200,
+                            'message' => 'Video actualizado',
+                            'video' => $video
+                        );                        
+                    }
+                }
+            }
+            
+            
+        }      
+     }
+    
+    public function detail(Request $request, $id = null){
+        $data = array(
+            'status' => 'error',
+            'code' => 404,
+            'message' => 'El video no se ha encontrado'
+        );     
+        
+        if($id != null){
+            $video = Video::find($id);
+            if(is_object($video)){
+                
+                $data = array(
+                    'status' => 'success',
+                    'code' => 200,
+                    'message' => 'Video encontrado',
+                    'video' => $video
+                );                 
+            }
+        }
+        else{            
+            $data['message'] = 'Id invalida';
+        }
+
+        return response()->json($data, $data['code']);
     }
     
-    public function listAll(){
-        $videos = Video::all();
-        var_dump($videos);
+    public function remove(Request $request, $id = null){
+         $data = array(
+            'status' => 'error',
+            'code' => 404,
+            'message' => 'El video no se ha encontrado'
+        );            
+         
+         if($id != null){
+                          
+            $video = Video::find($id);
+            if(is_object($video)){
+                $data = array(
+                  'status' => 'success',
+                  'code' => 200,
+                  'message' => 'Video encontrado',
+                  'video' => $video
+                );                                 
+            }
+         }
+         else{
+            $data['message'] = 'Id invalida';
+         }
+         
+         return response()->json($data, $data['code']);
     }
+    
+    public function listAll(Request $request){
+        $token = $request->header('Authorization');
+        $user = $jwtAuth->checkToken($token, true);
+        $id = $user->sub;
+        $videos = Video::where('user_id', $id)->orderBy('id', 'desc')->paginate(5);
+        return $videos;
+    }
+    
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -31,5 +31,6 @@ Route::post('/user/login', 'UserController@login');
 Route::put('/user/update', 'UserController@update');
 Route::get('/user/detail/{id}', 'UserController@detail');
 Route::post('/video/create', 'VideoController@create')->middleware(ApiAuthMiddleware::class);
-Route::get('/video', 'VideoController@listAll');
-Route::get('/video/detail/{id}', 'VideoController@detail');
+Route::post('/videos', 'VideoController@listAll')->middleware(ApiAuthMiddleware::class);
+Route::get('/video/detail/{id}', 'VideoController@detail')->middleware(ApiAuthMiddleware::class);
+Route::delete('/video/remove/{id}', 'VideoController@remove')->middleware(ApiAuthMiddleware::class);
